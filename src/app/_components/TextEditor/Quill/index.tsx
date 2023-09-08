b5225241c867ee788a16d87789c923683882ada1
KAIDI Mohammed <mohamed_kaidi@ext.connect-tech.sncf>
2023-09-08 14:37:39 +0200

    feat/ create a reusable button component

--- src/app/_components/TextEditor/Quill/index.tsx
+++ src/app/_components/TextEditor/Quill/index.tsx
@@ -1,11 +1,31 @@
 'use client';
-import dynamic from 'next/dynamic';
-import React, { useState } from 'react';
-const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+
+import React from 'react';
+import ReactQuill from 'react-quill';
+import EditorToolbar, { modules, formats } from './EditorToolbar';
+
 import 'react-quill/dist/quill.snow.css';
+import './styles.css';
 
-export default function Quill() {
-  const [value, setValue] = useState('');
+export const Editor = () => {
+  const [state, setState] = React.useState({ value: '' });
+  const handleChange = (value: any) => {
+    setState({ value });
+  };
 
-  return <ReactQuill theme="snow" value={value} onChange={setValue} />;
-}
+  return (
+    <div className="text-editor">
+      <EditorToolbar />
+      <ReactQuill
+        theme="snow"
+        value={state.value}
+        onChange={handleChange}
+        placeholder={'Write something awesome...'}
+        modules={modules}
+        formats={formats}
+      />
+    </div>
+  );
+};
+
+export default Editor;
