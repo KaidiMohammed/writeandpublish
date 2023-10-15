d18e0d2a6a692a608def3250d4ccb22876cfc588
kaidiMohammed <kaidi.mohammed1@gmail.com>
2023-10-15 13:14:17 +0200

    feat/add manifest file to manage pwa for different devices

--- src/app/_components/Post/index.tsx
+++ src/app/_components/Post/index.tsx
@@ -22,7 +22,7 @@ const Post = ({ title, content, author }: any) => {
         Author : {author}
       </p>
       <a
-        href={window.location.origin + '/hol'}
+        href={window.location.origin + '/post/write'}
         target="self"
         rel="noreferrer"
         className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -54,6 +54,27 @@ const Post = ({ title, content, author }: any) => {
       </a>
       <button onClick={() => handleClick()}>Clik me application</button>
       <button onClick={() => handleClickGoogle()}>Clik google</button>
+      <a
+        href={window.location.origin + '/post/write'}
+        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Read more wthout referrer
+        <svg
+          className="w-3.5 h-3.5 ml-2"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 10"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M1 5h12m0 0L9 1m4 4L9 9"
+          />
+        </svg>
+      </a>
     </div>
   );
 };
